24c8e84eb9639835a4272ff6ae6d3970295c2186
paulo campos <paulo.campos.dev@gmail.com>
2023-06-23 23:55:53 -0300

    fix tsx --watch

--- src/prisma/index.ts
+++ src/prisma/index.ts
@@ -2,7 +2,6 @@ import { PrismaClient } from '../../node_modules/.prisma/client/index'
 
 const prisma = new PrismaClient({
   errorFormat: 'pretty',
-  log: ['query', 'error']
 })
 
 export { prisma }
